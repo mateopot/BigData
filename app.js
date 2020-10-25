7c0d8d1cc677ddc0db86d0050fd3705561a2c7cc
Matéo <mateo.potin@etu.u-bordeaux.fr>
2020-10-25 16:33:27 +0100

    modifs

--- app.js
+++ app.js
@@ -115,29 +115,6 @@ app.get('/restaurants/average_rating', async (req, res) => {
     })
 });
 
-app.use(logErrors);
-app.use(clientErrorHandler);
-app.use(errorHandler);
-
-function logErrors(err, req, res, next) {
-    console.error(err.stack);
-    next(err);
-}
-
-function clientErrorHandler(err, req, res, next) {
-    if (req.xhr) {
-        res.status(500).send({ error: 'Something failed' });
-    } else {
-        next(err);
-    }
-}
-
-function errorHandler(err, req, res, next) {
-    res.status(500);
-    res.render('error', { error: err });
-}
-
-
 const PORT = 5000;
 
 app.listen(PORT, () => {
--- db.js
+++ db.js
@@ -14,56 +14,32 @@ async function main() {
 }
 
 async function getRestaurantById(restaurant_id) {
-    result = await client.db("masterclass_project").collection("restaurants")
+    return result = await client.db("masterclass_project").collection("restaurants")
         .findOne({ _id: ObjectId(restaurant_id) });
-
-    if (result) {
-        return result;
-    } else {
-        return 'Not finding data...';
-    }
 }
 
 async function updateRestaurant(restaurant_id, body) {
-    result = await client.db("masterclass_project").collection("restaurants")
+    return result = await client.db("masterclass_project").collection("restaurants")
         .updateOne(
             { _id: ObjectId(restaurant_id) },
             { $set: { name: body.name, long_coordinates: body.long_coordinates, lat_coordinates: body.lat_coordinates } }
         );
-
-    if (result) {
-        return `Edited restaurant #${restaurant_id}`;
-    } else {
-        return 'error...';
-    }
 }
 
 async function deleteRestaurant(restaurant_id) {
-    result = await client.db("masterclass_project").collection("restaurants")
+    return result = await client.db("masterclass_project").collection("restaurants")
         .deleteOne({ _id: ObjectId(restaurant_id) });
-
-    if (result) {
-        return `Deleted restaurant #${restaurant_id} successful.`;
-    } else {
-        return 'error...';
-    }
 }
 
 async function createRestaurant(body) {
-    result = await client.db("masterclass_project").collection("restaurants")
+    return result = await client.db("masterclass_project").collection("restaurants")
         .insertOne(
             { name: body.name, long_coordinates: body.long_coordinates, lat_coordinates: body.lat_coordinates }
         );
-
-    if (result) {
-        return `List of restaurants : #${result.insertedId}`;
-    } else {
-        return 'error...';
-    }
 }
 
 async function getAveragePrice() {
-    result = await client.db("masterclass_project").collection("restaurants")
+    return result = await client.db("masterclass_project").collection("restaurants")
         .aggregate(
             [
                 {
@@ -72,18 +48,12 @@ async function getAveragePrice() {
                 }
             ]
         ).toArray();
-
-    if (result) {
-        return `Average price for all the restaurants is ${result[0].avg_price}`;
-    } else {
-        return 'error...';
-    }
 }
 
 async function findRestaurantsWithLocation(long_coordinates, lat_coordinates, max_distance) {
     await client.db("masterclass_project").collection("restaurants").createIndex({ "location": "2dsphere" });
 
-    result = await client.db("masterclass_project").collection("restaurants")
+    return result = await client.db("masterclass_project").collection("restaurants")
         .find({
             location: {
                 $near: {
@@ -96,27 +66,15 @@ async function findRestaurantsWithLocation(long_coordinates, lat_coordinates, ma
                 }
             }
         }).toArray();
-
-
-    if (result) {
-        return result;
-    } else {
-        return 'error...';
-    }
 }
 
 async function getRestaurantRating(restaurant_id) {
 
-    result = await client.db("masterclass_project").collection("restaurants")
+    return result = await client.db("masterclass_project").collection("restaurants")
         .aggregate(
             [{ $project: { _id: "$_id", name: "$name", avg_stars: { $avg: "$reviews" } } }]
         ).toArray();
 
-    if (result) {
-        return result;
-    } else {
-        return 'error...';
-    }
 }
 
 main().catch(console.error);
